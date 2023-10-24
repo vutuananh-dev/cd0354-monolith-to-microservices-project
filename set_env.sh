8f95bacc1529bee00dfcdbd2a56dbf1ffa1efffc
Eugene Vu <vutuananh.dev@gmalil.com>
2023-10-24 14:04:03 +0700

    First commit by Eugene

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=anhvt43
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=udacity-2813642-db2.c8xtl8onm7wa.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacity
+export AWS_BUCKET=udacity-2813642-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,19 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
-
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
 
-  'dialect': config.dialect,
-  'storage': ':memory:',
+  dialect: config.dialect,
+  storage: ":memory:",
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
